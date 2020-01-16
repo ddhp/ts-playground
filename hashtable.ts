b96d312d200712e2aebd00522cfb50c91cb96e3f
ddhp <jessechen.jcc@gmail.com>
2020-01-16 18:10:00 +0900

    hash table

---
+++ hashtable.ts
@@ -0,0 +1,27 @@
+const H: (name: string, age: number, sex: string) => number = (name: string, age: number, sex: string): number => {
+    let sum = 0;
+    for (let i = 0; i < name.length; i++ ) {
+        sum += ASCI(name[i]);
+    }
+    sum += age;
+    sum += sex === 'M' ? 0 : 1;
+    return sum | 6;
+}
+
+// Hash Function
+// output is consistent with same input
+// return value is within a certain range
+// if H(x) !== H(y), then x must NOT equal to y
+// if H(x) === H(y) then we can explictly compare them
+//
+// Hash Table
+// an array which has object stored in the index generated from it's hash function
+//
+// Hash collision
+// since H(x) and H(y) can potentially output the same index
+// in this case, a hash collision happened
+// some ways to solve this
+// 1. separate chaining: each bucket is a linked list
+// 2. open addressing:
+//      - next key hash = H(k) + P(k, x) mod N where x is an increment value (x += 1) and N is the size of the table/array
+//      - hash function and probe functions are usually paired
